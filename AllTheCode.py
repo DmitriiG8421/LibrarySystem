d313051396870e6e458d3fb30bcb6c93fd7c75d2
DmitriiG8421 <golikov.dima15@gmail.com>
2025-03-30 14:42:41 +1100

    A small error

--- AllTheCode.py
+++ AllTheCode.py
@@ -12,7 +12,7 @@ class Library:
     def __init__(self):
         self.books = [Book("Quest Of The Sunfish - Escape To The Moon Islands", Author("Mardi McConnochie")),
                       Book("How To Survive On Mars", Author("Jasmina Lazendic-Galloway")),
-                      Book("One Piece- Volume 1"),Author("Eiichiro Oda"),
+                      Book("One Piece - Volume 1"),Author("Eiichiro Oda"),
                       Book("Harry Potter and the Philosopher's Stone",Author("J. K. Rowling")),
                       Book("The Lord of the Rings"),Author("John Ronald Reuel Tolkien")]
         
