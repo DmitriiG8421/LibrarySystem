c1a4f3e23029d22baaf208c3637e2e99c8dc08f4
DmitriiG8421 <golikov.dima15@gmail.com>
2025-04-05 11:09:32 +1100

    m

--- AllTheCode.py
+++ AllTheCode.py
@@ -1,13 +1,14 @@
 class Author:
-    def __init__(self, name):
+    def init(self, name):
         self.name = name
 
 
 class Book:
-    def __init__(self,title,author):
+    def init(self, isbn, title, author, counter):
         self.title = title
+        self.isbn = isbn
         self.author = author
-        self.isBorrowed = False
+        self.counter = counter
 
     def giveInfo(self):
         if self.isBorrowed == False:
@@ -15,41 +16,58 @@ class Book:
         if self.isBorrowed == True:
             print(f"{self.title} by {self.author} - borrowed")
 
-    def changeState(self, state):
-        self.isBorrowed = state
-
 
 class Library:
-    def __init__(self):
-        self.books = [Book("Quest Of The Sunfish - Escape To The Moon Islands", Author("Mardi McConnochie")),
-                      Book("How To Survive On Mars", Author("Jasmina Lazendic-Galloway")),
-                      Book("One Piece - Volume 1",Author("Eiichiro Oda")),
-                      Book("Harry Potter and the Philosopher's Stone",Author("J. K. Rowling")),
-                      Book("The Lord of the Rings",Author("John Ronald Reuel Tolkien"))]
+    def init(self):
+        self.shelf = [Book(isbn="1",
+                           title="Quest Of The Sunfish - Escape To The Moon Islands",
+                           author=Author("Mardi McConnochie"),
+                           counter=20),
+                      Book(isbn="2",
+                           title="How To Survive On Mars",
+                           author=Author("Jasmina Lazendic-Galloway"),
+                           counter=3),
+                      Book(isbn="3",
+                           title="One Piece - Volume 1",
+                           author=Author("Eiichiro Oda"),
+                           counter=30),
+                      Book(isbn="4",
+                           title="Harry Potter and the Philosopher's Stone",
+                           author=Author("J. K. Rowling"),
+                           counter=1),
+                      Book(isbn="5",
+                           title="The Lord of the Rings",
+                           author=Author("John Ronald Reuel Tolkien"),
+                           counter=9)]
+
+        self.bookByISBN = {book["isbn"]: book for book in self.shelf}
+
+    def borrowBook(self, isbn):
         
+        return self.bookByISBN.get(isbn)
 
     def displayBooks(self):
         print()
-        for i in range(len(self.books)):
-            userBook = self.books[i]
-            print(f"{i+1}.",end=" ")
+        for i in range(len(self.shelf)):
+            userBook = self.shelf[i]
+            print(f"{i + 1}.", end=" ")
             userBook.giveInfo()
         print()
-
 
 
 myLibrary = Library()
 print("Library menu:")
 while True:
-    userOption = input(" \nOption 1: Display the books.\n Option 2: Borrow a book.\n Option 3: Return a book.\n Option 4: Exit.\n Choose option(1-4):")
+    userOption = input(
+        " \nOption 1: Display the books.\n Option 2: Borrow a book.\n Option 3: Return a book.\n Option 4: Exit.\n Choose option(1-4):")
 
     if userOption == "1":
-        myLibrary.displayBooks()
+        myLibrary.dis2playBooks()
 
     if userOption == "2":
         myLibrary.displayBooks()
         userBook = int(input("Please enter the number for your book: "))
-        bookchosen = myLibrary.books[userBook-1]
+        bookchosen = myLibrary.shelf[userBook - 1]
         if bookchosen.isBorrowed == False:
             bookchosen.changeState(True)
             print(f"You have borrowed {bookchosen.title}.\n")
@@ -59,14 +77,14 @@ while True:
     if userOption == "3":
         myLibrary.displayBooks()
         userBook = int(input("Please enter the number for your book: "))
-        bookchosen = myLibrary.books[userBook-1]
+        bookchosen = myLibrary.shelf[userBook - 1]
         if bookchosen.isBorrowed == True:
             bookchosen.changeState(False)
             print(f"You have returned {bookchosen.title}.\n")
         else:
             print(f"{bookchosen} is not borrowed.")
 
-    if  userOption == "4":
+    if userOption == "4":
         print("\nClosing Library Menu...\n\n")
         exit()
 
